55f8456e9a4babe7db0f3cd6c5c111759d4857c1
vvilliam <wpalin@gmail.com>
2020-12-05 13:27:46 -0500

    style(massappct_u): Black

--- juriscraper/opinions/united_states/state/massappct_u.py
+++ juriscraper/opinions/united_states/state/massappct_u.py
@@ -25,23 +25,37 @@ class Site(OpinionSite):
 
     def build_url(self):
         start_date = self.case_date - timedelta(days=self.backwards_days)
-        url_template = 'https://128archive.com/?Action=search&DocketNumber=&PartyName=&ReleaseDateFrom=%s&ReleaseDateTo=%s&Keywords=&SortColumnName=Release+Date+Descending+Order&SortColumnName=Release+Date+Descending+Order&SortOrder=&CurrentPageNo=1&Pages=1&PageSize=100'
-        return url_template % (start_date.strftime("%m/%d/%Y"), self.case_date.strftime("%m/%d/%Y"))
+        url_template = "https://128archive.com/?Action=search&DocketNumber=&PartyName=&ReleaseDateFrom=%s&ReleaseDateTo=%s&Keywords=&SortColumnName=Release+Date+Descending+Order&SortColumnName=Release+Date+Descending+Order&SortOrder=&CurrentPageNo=1&Pages=1&PageSize=100"
+        return url_template % (
+            start_date.strftime("%m/%d/%Y"),
+            self.case_date.strftime("%m/%d/%Y"),
+        )
 
     def _download(self, request_dict={}):
         html = super(Site, self)._download(request_dict)
         return html
 
     def _get_download_urls(self):
-        return [link.get("href") for link in self.html.xpath('.//a[text()="Download PDF"]')]
+        return [
+            link.get("href")
+            for link in self.html.xpath('.//a[text()="Download PDF"]')
+        ]
 
     def _get_case_names(self):
         xp = './/div[contains(text(),"Case Name")]/following-sibling::div[1]'
-        return [case_name.text_content().strip() for case_name in self.html.xpath(xp)]
+        return [
+            case_name.text_content().strip()
+            for case_name in self.html.xpath(xp)
+        ]
 
     def _get_case_dates(self):
-        xp = './/div[contains(text(),"Release Date")]/following-sibling::div[1]'
-        return [convert_date_string(case_date.text_content().strip()) for case_date in self.html.xpath(xp)]
+        xp = (
+            './/div[contains(text(),"Release Date")]/following-sibling::div[1]'
+        )
+        return [
+            convert_date_string(case_date.text_content().strip())
+            for case_date in self.html.xpath(xp)
+        ]
 
     def _get_precedential_statuses(self):
         xp = './/div[contains(text(),"Case Name")]/following-sibling::div[1]'
@@ -49,5 +63,7 @@ class Site(OpinionSite):
 
     def _get_docket_numbers(self):
         xp = './/div[contains(text(),"Docket Number")]/following-sibling::div[1]'
-        return [docket_number.text_content().strip() for docket_number in self.html.xpath(xp)]
-
+        return [
+            docket_number.text_content().strip()
+            for docket_number in self.html.xpath(xp)
+        ]
